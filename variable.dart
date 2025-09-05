98d9e7aeb68893375ec16f2d1d41b47a16b18904
Fadhliakbar <104522615+fdhliakbar@users.noreply.github.com>
2025-09-05 14:51:49 +0700

    Add variable declarations and print statement

---
+++ variable.dart
@@ -0,0 +1,10 @@
+void main() {
+  // syntax variable= type variableName = value;
+  String name = "Fadhli";
+  String address = "Mars";  
+  num age = 20;
+  num height = 4.5;
+  bool isMarried = false;
+  
+  print("My name is $name")
+}
